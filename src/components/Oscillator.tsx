79ba865c76f00ae2587a9d612a7c905867c73d42
Dave Knapik <daveknapik@gmail.com>
2024-05-06 14:06:55 +0900

    Added radio buttons for changing Oscillator waveform.
    Gave up on making linter happy and ignored exhaustive-deps to avoid useEffect run-once complaints.
    Could extract into custom hook later per https://stackoverflow.com/questions/65693657/running-a-useeffect-once-with-happy-eslint.

--- src/components/Oscillator.tsx
+++ src/components/Oscillator.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useId, useEffect, useState } from "react";
 import * as Tone from "tone";
 
 interface OscillatorProps {
@@ -10,6 +10,7 @@ function Oscillator({ freqMin = 440, freqMax = 880 }: OscillatorProps) {
   // a Tone.Oscillator and its property
   const [osc, setOsc] = useState<Tone.Oscillator | undefined>();
   const [frequency, setFrequency] = useState(440);
+  const [waveform, setWaveform] = useState("sine");
 
   // a Tone.Channel and its properties
   const [channel, setChannel] = useState<Tone.Channel | undefined>();
@@ -18,15 +19,42 @@ function Oscillator({ freqMin = 440, freqMax = 880 }: OscillatorProps) {
 
   const [isPlaying, setIsPlaying] = useState(false);
 
+  const id = useId();
+
   useEffect(() => {
-    const o = new Tone.Oscillator(frequency, "sine");
+    const o = new Tone.Oscillator(
+      frequency,
+      waveform as Tone.ToneOscillatorType
+    );
     setOsc(o);
 
     const c = new Tone.Channel(volume, 0).toDestination();
     setChannel(c);
 
     o.connect(c);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  // update volume on the channel
+  useEffect(() => {
+    channel && channel.volume.setTargetAtTime(volume, 0, 0.01);
+  }, [channel, volume]);
+
+  // update pan on the channel
+  useEffect(() => {
+    channel && channel.pan.setTargetAtTime(pan, 0, 0.01);
+  }, [channel, pan]);
+
+  // update frequency on the osc
+  useEffect(() => {
+    osc && osc.frequency.setValueAtTime(frequency, 0.1);
+  }, [osc, frequency]);
+
+  // update waveform on the osc
+  useEffect(() => {
+    osc && (osc.type = waveform as Tone.ToneOscillatorType);
+  }, [osc, waveform]);
 
   const toggleAudio = (): void => {
     if (isPlaying) {
@@ -49,10 +77,7 @@ function Oscillator({ freqMin = 440, freqMax = 880 }: OscillatorProps) {
           min={freqMin}
           max={freqMax}
           value={frequency}
-          onChange={(e) => {
-            setFrequency(parseFloat(e.target.value));
-            osc?.frequency.setValueAtTime(frequency, 0.1);
-          }}
+          onChange={(e) => setFrequency(parseFloat(e.target.value))}
         />
         <div>{frequency}</div>
       </div>
@@ -65,13 +90,9 @@ function Oscillator({ freqMin = 440, freqMax = 880 }: OscillatorProps) {
           min="-80"
           max="0"
           value={volume}
-          onChange={(e) => {
-            setVolume(parseFloat(e.target.value));
-            if (channel) {
-              channel.volume.setTargetAtTime(volume, 0, 0.01);
-            }
-          }}
+          onChange={(e) => setVolume(parseFloat(e.target.value))}
         />
+        <div>{volume}</div>
       </div>
       <div className="flex items-center space-x-2">
         <label htmlFor="pan">P</label>
@@ -83,13 +104,50 @@ function Oscillator({ freqMin = 440, freqMax = 880 }: OscillatorProps) {
           max="1"
           value={pan}
           step="0.01"
-          onChange={(e) => {
-            setPan(parseFloat(e.target.value));
-            if (channel) {
-              channel.pan.setTargetAtTime(pan, 0, 0.01);
-            }
-          }}
+          onChange={(e) => setPan(parseFloat(e.target.value))}
         />
+        <div>{pan}</div>
+      </div>
+      <div className="flex items-center space-x-2">
+        <input
+          name={`waveform-${id}`}
+          type="radio"
+          id={`waveform-sine-${id}`}
+          value="sine"
+          checked={waveform === "sine"}
+          onChange={(e) => setWaveform(e.target.value)}
+        />
+        <label htmlFor={`waveform-sine-${id}`}>Sine</label>
+
+        <input
+          name={`waveform-${id}`}
+          type="radio"
+          id={`waveform-square-${id}`}
+          value="square"
+          checked={waveform === "square"}
+          onChange={(e) => setWaveform(e.target.value)}
+        />
+        <label htmlFor={`waveform-square-${id}`}>Square</label>
+
+        <input
+          name={`waveform-${id}`}
+          type="radio"
+          id={`waveform-triangle-${id}`}
+          value="triangle"
+          checked={waveform === "triangle"}
+          onChange={(e) => setWaveform(e.target.value)}
+        />
+        <label htmlFor={`waveform-triangle-${id}`}>Triangle</label>
+
+        <input
+          name={`waveform-${id}`}
+          type="radio"
+          id={`waveform-sawtooth-${id}`}
+          value="sawtooth"
+          checked={waveform === "sawtooth"}
+          onChange={(e) => setWaveform(e.target.value)}
+        />
+        <label htmlFor={`waveform-sawtooth-${id}`}>Saw</label>
       </div>
       <div className="text-center">
         <button onClick={toggleAudio}>{isPlaying ? "Stop" : "Start"}</button>
