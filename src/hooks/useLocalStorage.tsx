e666b3e888ff1d46ff7344a4869cd0e57772eef2
Dave Knapik <daveknapik@gmail.com>
2024-05-07 18:27:45 +0900

    Added useLocalStorage hook for use when migrating from light/dark theme binary to a full theme system.

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,24 @@
+// Based on https://blog.logrocket.com/using-localstorage-react-hooks/
+// localStorage-backed useState wrapper
+
+import { useState, useEffect } from "react";
+
+function getStorageValue(key: string, defaultValue: string) {
+  // getting stored value
+  const saved = localStorage.getItem(key) || "";
+  const initialValue: string = JSON.parse(saved);
+  return initialValue || defaultValue;
+}
+
+export const useLocalStorage = (key: string, defaultValue: string) => {
+  const [value, setValue] = useState(() => {
+    return getStorageValue(key, defaultValue);
+  });
+
+  useEffect(() => {
+    // storing input name
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
