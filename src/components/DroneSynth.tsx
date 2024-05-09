1d5974b9182461a6de622936b15ed3df617d13e5
Dave Knapik <daveknapik@gmail.com>
2024-05-09 22:08:58 +0900

    Created FrequencyRangeControl and moved freq range controls there

--- src/components/DroneSynth.tsx
+++ src/components/DroneSynth.tsx
@@ -1,4 +1,5 @@
 import Oscillator from "./Oscillator";
+import FrequencyRangeControl from "./FrequencyRangeControl";
 import { useState } from "react";
 
 function DroneSynth() {
@@ -40,23 +41,7 @@ function DroneSynth() {
           <div className="col-start-7">
             <button onClick={addOscillator}>+</button>
           </div>
-          <form className="col-span-6" onSubmit={handleFormSubmit}>
-            <input
-              className="w-20 mr-2 rounded-md border-0 px-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-              name="minFreq"
-              type="number"
-            />
-            <input
-              className="w-20 mr-2 rounded-md border-0 px-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-              name="maxFreq"
-              type="number"
-            />
-            <input
-              className="rounded-md bg-sky-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              type="submit"
-              value="freq!"
-            />
-          </form>
+          <FrequencyRangeControl handleFormSubmit={handleFormSubmit} />
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-12 my-5 place-items-center border-2 rounded border-pink-500 dark:border-sky-300 p-5">
           {oscillators}
---
+++ src/components/FrequencyRangeControl.tsx
@@ -0,0 +1,33 @@
+interface FrequencyRangeControlProps {
+  handleFormSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+}
+
+function FrequencyRangeControl({
+  handleFormSubmit,
+}: FrequencyRangeControlProps) {
+  return (
+    <form className="col-span-6" onSubmit={handleFormSubmit}>
+      <input
+        className="w-20 mr-2 rounded-md border-0 px-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+        max="20000"
+        min="1"
+        name="minFreq"
+        type="number"
+      />
+      <input
+        className="w-20 mr-2 rounded-md border-0 px-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+        max="20000"
+        min="1"
+        name="maxFreq"
+        type="number"
+      />
+      <input
+        className="rounded-md bg-sky-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        type="submit"
+        value="freq!"
+      />
+    </form>
+  );
+}
+
+export default FrequencyRangeControl;
