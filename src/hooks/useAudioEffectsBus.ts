c9e4b55dfa4490d07cfee7f1469cddaf3122f827
Dave Knapik <daveknapik@gmail.com>
2024-05-23 08:50:06 +0900

    Added documentation to connection hooks

--- src/hooks/useAudioEffectsBus.ts
+++ src/hooks/useAudioEffectsBus.ts
@@ -1,3 +1,11 @@
+/* 
+  Creates an audio effects bus (Tone.Channel)
+  and chains the audio effects passed in as an array to it, and then to the Tone.Destination.
+  This is useful for receivings audio sources (e.g., oscillators) at an effects bus
+
+  The bus is then returned as a ref.
+ */
+
 import * as Tone from "tone";
 import { useCallback, useEffect, useRef } from "react";
 import { AudioEffect } from "../types/AudioEffect";
--- src/hooks/useConnectChannelsToBus.ts
+++ src/hooks/useConnectChannelsToBus.ts
@@ -1,3 +1,9 @@
+/* 
+  This hook connects an array of channels to a bus.
+  It sends the channels to the bus and then connects them to the it.
+  This is useful for sending audio sources (e.g., oscillators) to their effects bus
+*/
+
 import * as Tone from "tone";
 
 export function useConnectChannelsToBus(
