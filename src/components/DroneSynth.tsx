db02afe2cdd20d51e4d14436a2c5dfc39cfb7429
Dave Knapik <daveknapik@gmail.com>
2024-05-26 16:13:18 +0900

    Added show/hide functionality to the main sections:   - Effects   - Polysynths   - Oscillators
    Added clsx to the project to conditionally apply classes to the sections.

--- src/components/DroneSynth.tsx
+++ src/components/DroneSynth.tsx
@@ -1,4 +1,5 @@
 import * as Tone from "tone";
+import { clsx } from "clsx";
 
 import AutoFilter from "./AutoFilter";
 import BitCrusher from "./BitCrusher";
@@ -23,6 +24,9 @@ import { OscillatorWithChannel } from "../types/OscillatorWithChannel";
 import { useState } from "react";
 import { usePolysynths } from "../hooks/usePolysynths";
 
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { MdKeyboardArrowDown } from "react-icons/md";
+
 interface DroneSynthProps {
   oscillatorCount?: number;
 }
@@ -31,6 +35,9 @@ function DroneSynth({ oscillatorCount = 6 }: DroneSynthProps) {
   const [minFreq, setMinFreq] = useState(440);
   const [maxFreq, setMaxFreq] = useState(454);
   const [playKeys] = useState<string[]>(["q", "w", "a", "s", "z", "x"]);
+  const [expandAudioEffects, setExpandAudioEffects] = useState(true);
+  const [expandPolysynths, setExpandPolysynths] = useState(true);
+  const [expandOscillators, setExpandOscillators] = useState(true);
 
   const beforeFilter = useAutoFilter();
   const bitCrusher = useBitCrusher();
@@ -98,6 +105,18 @@ function DroneSynth({ oscillatorCount = 6 }: DroneSynthProps) {
     }
   };
 
+  const toggleExpandAudioEffects = (): void => {
+    setExpandAudioEffects((prev) => !prev);
+  };
+
+  const toggleExpandPolysynths = (): void => {
+    setExpandPolysynths((prev) => !prev);
+  };
+
+  const toggleExpandOscillators = (): void => {
+    setExpandOscillators((prev) => !prev);
+  };
+
   return (
     <div className="dark:text-sky-300">
       <div className="border-2 rounded border-pink-500 dark:border-sky-300 pt-2 px-3">
@@ -112,8 +131,23 @@ function DroneSynth({ oscillatorCount = 6 }: DroneSynthProps) {
           />
         </div>
 
-        <div className="col-span-full justify-self-start mt-5">Effects</div>
-        <div className="grid grid-cols-1 gap-x-2 gap-y-3 md:grid-cols-2 my-5 border-2 rounded border-pink-500 dark:border-sky-300 p-5">
+        <div
+          className="flex items-center align-items-center mt-5"
+          onClick={toggleExpandAudioEffects}
+        >
+          {expandAudioEffects ? (
+            <MdKeyboardArrowDown />
+          ) : (
+            <MdKeyboardArrowRight />
+          )}
+          Effects
+        </div>
+        <div
+          className={clsx(
+            "grid grid-cols-1 gap-x-2 gap-y-3 md:grid-cols-2 my-5 border-2 rounded border-pink-500 dark:border-sky-300 p-5",
+            !expandAudioEffects && "hidden"
+          )}
+        >
           <AutoFilter filter={beforeFilter} />
           <BitCrusher bitCrusher={bitCrusher} />
           <Chebyshev chebyshev={chebyshev} />
@@ -123,15 +157,45 @@ function DroneSynth({ oscillatorCount = 6 }: DroneSynthProps) {
           <EffectsBusSendControl bus={mainAudioEffectsBus} />
         </div>
 
-        <div className="col-span-full justify-self-start">PolySynth</div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 my-5 place-items-center border-2 rounded border-pink-500 dark:border-sky-300 p-5">
+        <div
+          className="flex items-center align-items-center mt-5"
+          onClick={toggleExpandPolysynths}
+        >
+          {expandPolysynths ? (
+            <MdKeyboardArrowDown />
+          ) : (
+            <MdKeyboardArrowRight />
+          )}
+          PolySynth
+        </div>
+        <div
+          className={clsx(
+            "grid grid-cols-1 md:grid-cols-2 gap-12 my-5 place-items-center border-2 rounded border-pink-500 dark:border-sky-300 p-5",
+            !expandPolysynths && "hidden"
+          )}
+        >
           {polysynths.map((polysynth, i) => (
             <PolySynth key={i} polySynth={polysynth} />
           ))}
         </div>
 
-        <div className="col-span-full justify-self-start">Oscillators</div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 my-5 place-items-center border-2 rounded border-pink-500 dark:border-sky-300 p-5">
+        <div
+          className="flex items-center align-items-center my-5"
+          onClick={toggleExpandOscillators}
+        >
+          {expandOscillators ? (
+            <MdKeyboardArrowDown />
+          ) : (
+            <MdKeyboardArrowRight />
+          )}
+          Oscillators
+        </div>
+        <div
+          className={clsx(
+            "grid grid-cols-1 md:grid-cols-2 gap-12 my-5 place-items-center border-2 rounded border-pink-500 dark:border-sky-300 p-5",
+            !expandOscillators && "hidden"
+          )}
+        >
           {oscillators.map((oscillator, i) => (
             <Oscillator
               playPauseKey={playKeys[i]}
