4b728e40ae6b152b4455d61defce33e71ef15832
Dave Knapik <daveknapik@gmail.com>
2024-06-02 20:29:01 +0900

    - Recording now works in Safari and Chrome - Simplified controls to Start/Stop Recording and Download buttons (no playback controls) - Wrapped Recorder in its own collapsable section - Effects and Polysynth sections are collapsed by default

--- src/components/Recorder.tsx
+++ src/components/Recorder.tsx
@@ -1,5 +1,5 @@
 import * as Tone from "tone";
-import { useState, MutableRefObject } from "react";
+import { useState, MutableRefObject, Fragment } from "react";
 
 interface RecorderProps {
   recorder: MutableRefObject<Tone.Recorder | null>;
@@ -24,14 +24,32 @@ function Recorder({ recorder }: RecorderProps) {
 
   return (
     <div>
-      <button onClick={() => void handleRecord()}>
-        {isRecording ? "Stop" : "Start"}
-      </button>
-      <audio controls preload="none" src={url}></audio>
+      <div className="flex items-center align-items-center">
+        <button
+          onClick={() => void handleRecord()}
+          className="mr-3 border-2 rounded border-pink-500 dark:border-sky-300 px-3"
+        >
+          {isRecording ? "Stop" : "Start"}
+        </button>
+        {url && !isRecording && (
+          <Fragment>
+            <audio preload="none" src={url} className="mr-3"></audio>
+
+            <a
+              download={`tone-drone-recording${Date.now().toString()}`}
+              href={url}
+              className="mr-3 border-2 rounded border-pink-500 dark:border-sky-300 px-3"
+            >
+              Download
+            </a>
+          </Fragment>
+        )}
+      </div>
       {url && !isRecording && (
-        <a download={`tone-drone-recording${Date.now().toString()}`} href={url}>
-          Download
-        </a>
+        <div className="text-sm mt-2">
+          Note: If you start a new recording, you will lose the current one.
+          Please download it first.
+        </div>
       )}
     </div>
   );
