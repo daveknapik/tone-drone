946cf50ebdcc3494df4d87112f4321cb3d368dbc
Dave Knapik <daveknapik@gmail.com>
2024-06-07 09:27:33 +0900

    Converted Reverb to default to Tone.js object instead of null

--- src/hooks/useReverb.ts
+++ src/hooks/useReverb.ts
@@ -1,20 +1,14 @@
 import * as Tone from "tone";
-import { useEffect, useRef, MutableRefObject } from "react";
+import { useRef } from "react";
 
 export function useReverb() {
-  const reverb = useRef<Tone.Freeverb>(null) as MutableRefObject<Tone.Freeverb>;
-
-  useEffect(() => {
-    reverb.current = new Tone.Freeverb({
+  const reverb = useRef<Tone.Freeverb>(
+    new Tone.Freeverb({
       dampening: 1000,
       roomSize: 0.95,
       wet: 0,
-    });
-
-    return () => {
-      reverb.current.dispose();
-    };
-  }, []);
+    })
+  );
 
   return reverb;
 }
