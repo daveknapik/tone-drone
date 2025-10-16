6920795bd289c5db7430ad3339c9a7c14b2d990b
Dave Knapik <daveknapik@gmail.com>
2025-10-16 23:44:07 +0900

    Added GitHub Actions for basic CI (test.yml) and CD (deploy.yml). Added .nvmrc to specify node version.

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,12 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/e2e/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
+    ],
   },
 });
